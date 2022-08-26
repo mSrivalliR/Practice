332001049731be8894a658653a6c6697d18dd3bf
Admin <vallimellacheruvu21@gmail.com>
2022-08-26 12:39:25 +0530

    firstmessage

---
+++ src/main/java/Automation/NewPractice/App.java
@@ -0,0 +1,7 @@
+package Automation.NewPractice;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
---
+++ src/test/java/Automation/NewPractice/AppTest.java
@@ -0,0 +1,13 @@
+package Automation.NewPractice;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
